46e8fc8e9459160bd75233747559e170d008d213
Joost van Dijk <vandijk.joost@gmail.com>
2023-02-24 13:02:11 +0100

    symlink

--- provision.sh
+++ provision.sh
@@ -1,14 +1,1 @@
-apt update
-apt upgrade -y
-# hsm
-apt install -y softhsm
-softhsm2-util --init-token --label "Dogtag" --so-pin Secret.123 --pin Secret.123 --free
-# patch: /usr/lib/python3/dist-packages/pki/server/deployment/scriptlets/configuration.py
-# check already patched: /usr/lib/python3/dist-packages/pki/server/deployment/pkiparser.py 
-# ds
-apt install -y 389-ds-base
-dscreate from-file /vagrant/ds.inf
-ldapadd -H ldap://ubuntu-jammy  -x -D "cn=Directory Manager" -w Secret.123 -f /vagrant/example.ldif
-# ca
-apt install -y dogtag-pki
-pkispawn -f /vagrant/softhsm-ca.cfg -s CA
+softhsm.sh
